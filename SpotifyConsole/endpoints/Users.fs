f7ae87bcb04bae3a908ca16b69cf2e4e76f4301c
attilio.dinunno <attilio.dinunno@gmail.com>
2025-11-08 20:17:10 +0100

    Added first User endpoint

---
+++ SpotifyConsole/endpoints/Users.fs
@@ -0,0 +1,40 @@
+module SpotifyConsole.Users
+
+open Base
+
+let getUsersTopItemsAsync (query: list<string>) =
+    let urlMapping =
+        query
+        |> List.mapi (fun i s ->
+            match i with
+            | 0 ->
+                match s with
+                | "artists" -> sprintf "%s?" s
+                | "tracks" -> sprintf "%s?" s
+                | _ -> "tracks?"
+            | 1 ->
+                match s with
+                | "long" -> sprintf "time_range=%s_term" s
+                | "medium" -> sprintf "time_range=%s_term" s
+                | "short" -> sprintf "time_range=%s_term" s
+                | _ -> "time_range=medium_term"
+            | 2 ->
+                match parseIntStrOption s with
+                | Some n when n > 0 && n <= 50 -> sprintf "&limit=%s" s
+                | _ -> ""
+            | 3 ->
+                match parseIntStrOption s with
+                | Some n when n >= 0 -> sprintf "&offset=%s" s
+                | _ -> ""
+            | _ -> "")
+
+    let url =
+        if query.Length > 0 then
+            printfn "Sending request with parameters %A" urlMapping
+
+            urlMapping
+            |> List.fold (fun (out: string) (next: string) -> out + next) (sprintf "%s/me/top/" BASE_URL)
+        else
+            failwith "Query is missing required parameters"
+
+    sendGetRequest url
